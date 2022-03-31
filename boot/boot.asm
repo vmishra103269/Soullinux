755c3c894d5883184d5c5d4e96b1d3a92d027703
Venkatesh Mishra <100060154+vmishra103269@users.noreply.github.com>
2022-03-31 08:55:02 +0530

    Create boot.asm

---
+++ boot/boot.asm
@@ -0,0 +1,29 @@
+;(C) Copyright 2022 Venkatesh Mishra
+;file: boot.asm
+;author: Venkatesh Mishra
+;pl: x86 Assembly
+;Project: Soul Linux boot sector
+;api used: none
+;date created: NA
+;time created: NA
+
+[org 0x7c00]
+mov ah, 0x0e
+mov bx, variableName
+
+printString:
+	mov al, [bx]
+	cmp al, 0
+	je end
+	int 0x10
+	inc bx
+	jmp printString
+end:
+
+jmp $
+
+variableName: 
+	db "Welcome to Soul Linux!", 0
+	
+times 510-($-$$) db 0
+dw 0xaa5
