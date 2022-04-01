e550023fc49372e4a2e2d999fb462b3084a2081c
Venkatesh Mishra <100060154+vmishra103269@users.noreply.github.com>
2022-04-01 11:13:49 +0530

    Update keyboard.rs

--- installer/keyboard.rs
+++ installer/keyboard.rs
@@ -1,7 +1,16 @@
+/*
+|#######################################|
+|## Soullinux Keyboard config         ##|
+|## (C) Copyright Venkatesh Mishra    ##|
+|## Under the GNU GPL v3 License      ##|
+|## date: 31/03/2022                  ##|
+########################################|
+*/
+
 use std::io::Command;
 
 fn main() {
-    let the_output = Command::new("ps").arg("aux").output()
+    let the_output = Command::new("localectl").arg("set-keymap en").output()
         .ok().expect("Failed to execute.");
     let encoded = String::from_utf8_lossy(the_output.output.as_slice());
     print!("{}", encoded);
